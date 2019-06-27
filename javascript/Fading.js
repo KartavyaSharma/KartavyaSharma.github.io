6f9f3ed9eb5063d8df234e6065b9acac55e3211b
Kartavya Sharma <Kartavya@outlook.com>
2019-06-27 15:19:03 +0530

    Gradient Testing

--- javascript/Fading.js
+++ javascript/Fading.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if($(this).scrollTop() > 0){
+        if($(this).scrollTop() > 50){
             $(".logo").css({"opacity" : "0"})
         }
         else {
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $(window).scroll(function(){
-        if($(this).scrollTop() > 0){
+        if($(this).scrollTop() > 50){
             $(".nav-wrapper").css({"opacity" : "0"})
         }
         else {
