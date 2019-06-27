c98be32fa0badb730ca1a4d564638de3edf2597f
Kartavya Sharma <Kartavya@outlook.com>
2019-06-27 23:05:49 +0530

    Width Change

--- javascript/Fading.js
+++ javascript/Fading.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if($(this).scrollTop() > 20){
+        if($(this).scrollTop() > 5){
             $(".logo").css({"opacity" : "0"})
         }
         else {
@@ -21,7 +21,7 @@ var y = window.matchMedia("(max-width: 864px)")
 
 $(document).ready(function(){
     $(window).scroll(function(){
-        if($(this).scrollTop() > 20 && myFunctionY(y)){
+        if($(this).scrollTop() > 5 && myFunctionY(y)){
             $(".nav-wrapper").css({"opacity" : "0"})
         }
         else {
