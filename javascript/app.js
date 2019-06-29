a489e66f19bb64514814f7f165b98b14f8a6d516
Kartavya Sharma <Kartavya@outlook.com>
2019-06-29 22:14:12 +0530

    Index Description

--- javascript/app.js
+++ javascript/app.js
@@ -7,10 +7,40 @@ function closeNav() {
     document.getElementById('myNav').style.width = "0%";
 }
 
-//Hover to see
-// document.getElementsByTagName('a').onmouseover = function() {
-//     document.getElementsByClassName('in-overlay').style.visibility = "visible";
-// }
-// document.getElementsByClassName('topic-list').onmouseout = function() {
-//     document.getElementsByClassName('in-overlay').style.visibility = "hidden";
-// }
+$(".listS").hover(function(){
+    $(this).find(".in-overlay").css({"visibility":"visible"});
+}, function(){
+    $(this).find(".in-overlay").css({"visibility":"hidden"});
+});
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 5){
+            $(".logo").css({"opacity" : "0"})
+        }
+        else {
+            $(".logo").css({"opacity" : "1"})
+        }
+    })
+})
+
+function myFunctionY (y) {
+    if (y.matches) {
+        $(".nav-wrapper").css({"opacity" : "1"})
+        return false
+    } else {
+        return true
+    }
+}
+var y = window.matchMedia("(max-width: 864px)")
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 5 && myFunctionY(y)){
+            $(".nav-wrapper").css({"opacity" : "0"})
+        }
+        else {
+            $(".nav-wrapper").css({"opacity" : "1"})
+        }
+    })
+})
--- javascript/hover.js
+++
@@ -1,5 +0,0 @@
-$(".listS").hover(function(){
-    $(this).find(".in-overlay").css({"visibility":"visible"});
-}, function(){
-    $(this).find(".in-overlay").css({"visibility":"hidden"});
-});
