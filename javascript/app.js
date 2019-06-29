fdb972cd869528862af88a8ea91708e707fb1879
Kartavya Sharma <Kartavya@outlook.com>
2019-06-29 12:20:49 +0530

    Updated CSS, index, main JS
    Animations for card menu and content test

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+// Card Popup
+function openNav() {
+    document.getElementById('myNav').style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById('myNav').style.width = "0%";
+}
